88c3117287140cee39e4be38f480b9a2031e5680
anderswodenker <me@myworkadaylife.de>
2018-09-07 14:30:00 +0200

    add vue.js

---
+++ frontend/js/script.js
@@ -0,0 +1,35 @@
+import '../scss/style.scss';
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import jQuery from 'jquery';
+Vue.use(VueResource);
+const http=Vue.http;
+
+export default http
+
+!function ($) {
+
+}(jQuery);
+var app = new Vue({
+    el: '#app',
+    data: {
+        path: "",
+        resultData: "",
+        loading: false,
+        show: false,
+    },
+    methods: {
+        post: function () {
+            this.show = false;
+            this.loading = true;
+            this.$http.post("/", {
+                path: this.path
+            }).then(response => {
+                this.loading = false;
+                this.resultData = response.body;
+                this.show = true;
+            }, response => {
+                console.log("error");
+            });
+        }}
+});
